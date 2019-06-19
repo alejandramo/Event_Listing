ca394313e6fb612f220b8965a2b5db42b8415824
alejandramo <alejandra.momari@gmail.com>
2019-06-19 01:52:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
+import Navigation from './Components/Navigation/Navigation.jsx';
+import CardEvent from './Components/CardEvent/CardEvent';
+import Footer from './Components/Footer/Footer';
+
+import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navigation/>
+        <CardEvent/>
+        <Footer/>>
+  
     </div>
   );
 }
---
+++ src/Components/CardEvent/CardEvent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CardEvent =() => {
+    return (
+
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className='f3 link dim black underline pa3 pointer'>Sign out</p>
+        </nav>
+    )
+}
+
+
+
+export default CardEvent;
